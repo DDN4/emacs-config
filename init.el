c5bb2fe9938d8ad1a2079334232cfc284085706f
Dan Nemeth <dnemeth@pivotal.io>
2015-01-31 21:32:09 -0500

    Org-mode, Magit, Smyx

--- init.el
+++ init.el
@@ -1,3 +1,20 @@
 (package-initialize)
 (require 'ob-tangle)
 (org-babel-load-file "~/.emacs.d/emacs.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (smyx)))
+ '(custom-safe-themes
+   (quote
+    ("0f002f8b472e1a185dfee9e5e5299d3a8927b26b20340f10a8b48beb42b55102" default)))
+ '(org-agenda-files (quote ("~/org/pse.org" "~/org/tda.org" "~/org/gtd.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
