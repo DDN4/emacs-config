6df6ceb5c1278eff8fedd8bb5f9c6c993b241274
Dan Nemeth <dnemeth@pivotal.io>
2015-02-10 13:23:04 -0500

    Golang for Babel, GFM export, minor changes for stability

--- init.el
+++ init.el
@@ -11,10 +11,13 @@
  '(custom-safe-themes
    (quote
     ("0f002f8b472e1a185dfee9e5e5299d3a8927b26b20340f10a8b48beb42b55102" default)))
- '(org-agenda-files (quote ("~/org/pse.org" "~/org/tda.org" "~/org/gtd.org"))))
+ '(org-agenda-files
+   (quote
+    ("~/org/tda.org" "~/org/gtd.org" "~/org/journal.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
