a8acc61f5a90c051d50a870d18a997f03c656f0c
Dan Nemeth <dnemeth@pivotal.io>
2015-09-09 16:29:01 -0400

    Improved evil-mode settings.

--- init.el
+++ init.el
@@ -7,13 +7,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282828" "#FAB1AB" "#D1FA71" "#FFA600" "#7b68ee" "#dc8cc3" "#96D9F1" "#F7F7F7"])
  '(custom-enabled-themes (quote (smyx)))
  '(custom-safe-themes
    (quote
-    ("0f002f8b472e1a185dfee9e5e5299d3a8927b26b20340f10a8b48beb42b55102" default)))
+    ("51277c9add74612c7624a276e1ee3c7d89b2f38b1609eed6759965f9d4254369" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "0f002f8b472e1a185dfee9e5e5299d3a8927b26b20340f10a8b48beb42b55102" default)))
+ '(fci-rule-color "#151515")
+ '(livedown:autostart nil)
+ '(livedown:open t)
+ '(livedown:port 1337)
  '(org-agenda-files
    (quote
-    ("~/org/tda.org" "~/org/gtd.org" "~/org/journal.org"))))
+    ("~/org/gtd.org" "~/org/journal.org" "~/org/pez.org" "~/org/travelers.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
